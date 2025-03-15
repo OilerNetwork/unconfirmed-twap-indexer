d766ee3c8b9837179e1b1725b854e328691f4399
Dhruv Gupta <dhruv035@gmail.com>
2025-03-15 06:30:20 +0530

    MAINNET

--- src/runner.ts
+++ src/runner.ts
@@ -30,7 +30,7 @@ export class Runner {
     this.viemClient = createPublicClient({
       chain: mainnet,
       transport: webSocket(
-        `wss://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`
+        `wss://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`
       ),
     });
   }
